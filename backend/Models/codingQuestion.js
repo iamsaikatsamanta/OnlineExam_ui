3d8cdb19cc63fec57c9668a58d10de9293eac2ad
Saikat Samanta <iamsaikatsamanta@gmail.com>
2019-01-01 02:11:06 +0530

    Ui Fixed

--- backend/Models/codingQuestion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var codingQuestionSchema = mongoose.Schema({
-    question: {type: String, required: true},
-    input: {type: Array, required: true},
-    output: {type: Array, required: true}
-});
-
-module.exports = mongoose.model("CodingQuestion",codingQuestionSchema);
--- backend/Models/regQuestion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var regQSchema = mongoose.Schema({
-    question: {type: String, required: true},
-    option: {type: Array, required: true},
-    correct: {type: String, required: true}
-});
-
-module.exports = mongoose.model("RegQuestion",regQSchema);
